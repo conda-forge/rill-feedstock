585a31a779a629b6265bb19a25b4783d43627e2a
Jan-Benedikt Jagusch <jan.jagusch@gmail.com>
2023-12-21 17:27:21 +0000

    Fix build.

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,7 @@ set -exuo pipefail
 
 pushd cli
 go-licenses save . --save_path ../library_licenses --ignore modernc.org/mathutil --ignore go.uber.org/zap/exp/zapslog
+popd
 
 # Don't use bundled pre-built static libs
 export GOFLAGS='-tags=duckdb_use_lib,dynamic'
@@ -11,8 +12,7 @@ go build \
     -v \
     -o "${PREFIX}"/bin/rill \
     -ldflags="-X 'main.version=${PKG_VERSION}'"
-
-popd
+    cli/main.go 
 
 # Clear out cache to avoid file not removable warnings
 chmod -R u+w $(go env GOPATH) && rm -r $(go env GOPATH)
